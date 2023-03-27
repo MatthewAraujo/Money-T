29fbae31cda1a5cd56551917993324bd645f6dd8
Matthew Araujo <matthewaraujo20@gmail.com>
2023-03-27 17:51:24 -0300

    finalizei

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,6 @@
 import { useContextSelector } from "use-context-selector";
 import { Header } from "../../components/Header";
-import { Summary } from "../../components/NewTransactionModal/Summary";
+import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import SearchForm from "./components/SearchForm";
